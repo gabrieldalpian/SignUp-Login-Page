5451a82fb93fabf9ccbad508116b97b421334269
Dalpian77 <gabrieldalpianpedro@gmail.com>
2025-07-17 12:28:05 -0400

    fetching frontend

--- src/main/java/com/example/login_auth/security/Cors.java
+++ src/main/java/com/example/login_auth/security/Cors.java
@@ -10,7 +10,7 @@ public class Cors implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://sign-up-login-page-two.vercel.app")
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS");
     }
 }
